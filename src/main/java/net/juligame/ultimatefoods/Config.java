c862c4228a6f35f9a9d31c6641c8ca18080110ad
7Dev <cammaratajuligame@gmail.com>
2025-09-04 22:48:59 -0300

    Update resource pack URL and increment version to 1.4.0 in plugin.yml

--- src/main/java/net/juligame/ultimatefoods/Config.java
+++ src/main/java/net/juligame/ultimatefoods/Config.java
@@ -6,7 +6,7 @@ import dev.juligame.Jconfig.JConfig;
 /*    */ 
 /*    */ public class Config extends JConfig {
 /*    */   public boolean sendResourcePack = true;
-/*  7 */   public String resourcePackURL = "https://drive.google.com/uc?export=download&id=1zeHKE3A6azBuRy6xVFbYtPYLiP4AtObK";
+/*  7 */   public String resourcePackURL = "https://drive.google.com/uc?export=download&id=1oFhJEaFS1x2vKgfSfRc1YbqaUXvSDSOZ"; // https://drive.google.com/file/d/1oFhJEaFS1x2vKgfSfRc1YbqaUXvSDSOZ/view?usp=drive_link
 /*    */   
 /*    */   public Config(String path) {
 /* 10 */     super(path, "");
